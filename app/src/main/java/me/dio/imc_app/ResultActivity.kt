e401e97112f482f1f4a3817dd1c4846b05605413
Ully Machado <103787911+UllyMachado@users.noreply.github.com>
2022-07-13 01:39:14 -0300

    Initial commit

---
+++ app/src/main/java/me/dio/imc_app/ResultActivity.kt
@@ -0,0 +1,12 @@
+package me.dio.imc_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+    //TODO: Implementar exibição das faixas de peso, tratamento de erro (pop-up), adição do cálculo por BMI, animações.
+}
